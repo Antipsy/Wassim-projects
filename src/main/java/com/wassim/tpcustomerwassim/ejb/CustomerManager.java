9f6b5ceae3602127f0832d3d43554762cb8e4234
Antipsy <wassim.elbakbachi@emsi-edu.ma>
2022-12-10 21:53:35 +0100

    version final

--- src/main/java/com/wassim/tpcustomerwassim/ejb/CustomerManager.java
+++ src/main/java/com/wassim/tpcustomerwassim/ejb/CustomerManager.java
@@ -2,8 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ejb;
-import entities.Customer;
+package com.wassim.tpcustomerwassim.ejb;
+import com.wassim.tpcustomerwassim.entities.Customer;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
---
+++ src/main/java/com/wassim/tpcustomerwassim/jsf/CustomerBean.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.wassim.tpcustomerwassim.jsf;
+
+import com.wassim.tpcustomerwassim.ejb.CustomerManager;
+import com.wassim.tpcustomerwassim.entities.Customer;
+import jakarta.ejb.EJB;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author Wassim
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+
+    private List<Customer> customerList;  
+
+  @EJB
+  private CustomerManager customerManager;  
+        
+  public CustomerBean() {  }  
+        
+  /** 
+   * Retourne la liste des clients pour affichage dans une DataTable 
+   * @return 
+   */  
+  public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  }  
+}
