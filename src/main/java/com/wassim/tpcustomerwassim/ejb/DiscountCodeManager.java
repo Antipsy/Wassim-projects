3089d06d8bcd1548d4a74cadec5b8ecdbbbe414d
Antipsy <wassim.elbakbachi@emsi-edu.ma>
2022-12-14 12:43:24 +0100

    version final

---
+++ src/main/java/com/wassim/tpcustomerwassim/ejb/DiscountCodeManager.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.wassim.tpcustomerwassim.ejb;
+
+import com.wassim.tpcustomerwassim.entities.DiscountCode;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+
+/**
+ *
+ * @author Wassim
+ */
+@Stateless
+public class DiscountCodeManager {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<DiscountCode> getAllDiscountCodes() {
+        Query query = em.createNamedQuery("DiscountCode.findAll");
+        return query.getResultList();
+    }
+
+    public DiscountCode findById(String discountCode) {
+        return em.find(DiscountCode.class, discountCode);
+    }
+
+    public void persist(DiscountCode discountCode) {
+        em.persist(discountCode);
+    }
+}
---
+++ src/main/java/com/wassim/tpcustomerwassim/jsf/CustomerDetailsBean.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.wassim.tpcustomerwassim.jsf;
+
+import com.wassim.tpcustomerwassim.ejb.CustomerManager;
+import com.wassim.tpcustomerwassim.ejb.DiscountCodeManager;
+import com.wassim.tpcustomerwassim.entities.Customer;
+import com.wassim.tpcustomerwassim.entities.DiscountCode;
+import jakarta.ejb.EJB;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class CustomerDetailsBean implements Serializable {
+  private int idCustomer;
+  private Customer customer;
+
+  @EJB
+  private CustomerManager customerManager;
+  @EJB
+  private DiscountCodeManager discountCodeManager;
+
+  public int getIdCustomer() {
+    return idCustomer;
+  }
+  /**
+   * Retourne la liste de tous les DiscountCode.
+     * @return 
+   */
+  public List<DiscountCode> getDiscountCodes() {
+    return discountCodeManager.getAllDiscountCodes();
+  }
+  
+  /**
+   * getter pour la propriété discountCodeConverter.
+     * @return 
+   */              
+  public Converter<DiscountCode> getDiscountCodeConverter() {
+    return new Converter<DiscountCode>() {
+      /**
+       * Convertit une String en DiscountCode.
+       *
+       * @param value valeur à convertir
+       */
+      @Override
+      public DiscountCode getAsObject(FacesContext context, UIComponent component, String value) {
+        return discountCodeManager.findById(value);
+      }
+
+      /**
+       * Convertit un DiscountCode en String.
+       *
+       * @param value valeur à convertir
+       */
+      @Override
+      public String getAsString(FacesContext context, UIComponent component, DiscountCode value) {
+        return value.getDiscountCode();
+      }
+    };
+  }
+  
+  public void setIdCustomer(int idCustomer) {
+    this.idCustomer = idCustomer;
+  }
+
+  /**
+   * Retourne les détails du client courant (contenu dans l'attribut customer de
+   * cette classe).
+     * @return 
+   */
+    public Customer getDetails() {
+      return customer;
+    }
+
+  /**
+   * Action handler - met à jour dans la base de données les données du client
+   * contenu dans la variable d'instance customer.
+   * @return la prochaine page à afficher, celle qui affiche la liste des clients.
+   */
+  public String update() {
+    // Modifie la base de données.
+    // Il faut affecter à customer (sera expliqué dans le cours).
+    customer = customerManager.update(customer);
+    return "customerList";
+  }
+
+  public void loadCustomer() {
+    this.customer = customerManager.getCustomer(idCustomer);
+  }
+}
